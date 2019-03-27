6d11fb02b32ba6923a86ecfaf1bc34b905b65992
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-03-27 07:35:14 -0700

    [#164584657] Add test listing to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# sample_listing.json is a copy of the listing API endpoint response
+# returned by Salesforce in the original DAHLIA. we're using it as a
+# quick way to get some test listing data while SMC/SJ DAHLIA is in
+# early development.
+sample_listing_file = File.open 'lib/sample_data/sample_listing.json'
+sample_listing_salesforce_fields = JSON.parse(sample_listing_file)
+listing = Listing.new
+listing.add_fields(sample_listing_salesforce_fields, :salesforce)
+Listing.create listing.to_domain.to_hash
