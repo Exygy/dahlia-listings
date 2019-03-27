629ca4c6151b5630eb7f9791afc15eda6342b828
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-03-27 12:47:34 -0700

    [#164584657] Remove cred env vars from app.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 # returned by Salesforce in the original DAHLIA. we're using it as a
 # quick way to get some test listing data while SMC/SJ DAHLIA is in
 # early development.
-sample_listing_file = File.open 'lib/sample_data/sample_listing.json'
+sample_listing_file = File.read 'lib/sample_data/sample_listing.json'
 sample_listing_salesforce_fields = JSON.parse(sample_listing_file)
 listing = Listing.new
 listing.add_fields(sample_listing_salesforce_fields, :salesforce)
