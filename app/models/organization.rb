ba3485bf52801f18e998a37af7b889a0ddf73bdd
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-04-18 10:17:47 -0700

    [#165425568] Make all Heroku app domains associated with test SMC org

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,7 @@
 require 'safe_yaml'
 
+# Represents a group that owns listings - e.g. a county, a city,
+# or a housing developer
 class Organization
   def self.lookup_initials=(value)
     Thread.current['lookup_initials'] = value
@@ -9,9 +11,11 @@ class Organization
     Thread.current['lookup_initials']
   end
 
+  # rubocop:disable Style/RedundantSelf
   def self.t(lookup_name, *args)
     I18n.t("#{self.lookup_initials}.#{lookup_name}", *args)
   end
+  # rubocop:enable Style/RedundantSelf
 
   def self.load_domain_information
     SafeYAML.load_file(File.join(Rails.application.root, 'config', 'organizations.yml'))
@@ -19,13 +23,12 @@ class Organization
 
   def self.find_information_for_domain(domain)
     org_info = load_domain_information
-    found_org = org_info.find{|k,v| v['domain'] == domain}
-    if found_org
-      org_data = found_org[1]
-      org_data['initials'] = found_org[0]
-      org_data
-    else
-      raise RuntimeError, "No domain information could be found"
-    end
+    found_org = org_info.find { |_k, v| domain.include? v['domain'] }
+
+    raise 'No domain information could be found' unless found_org
+
+    org_data = found_org[1]
+    org_data['initials'] = found_org[0]
+    org_data
   end
 end
