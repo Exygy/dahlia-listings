626fa271ab02d5da80bfd8714f46d470ddd81d06
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-04-19 10:32:03 -0700

    [#164586110] Add more test listing data to DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# sample_listing.json is a copy of the listing API endpoint response
-# returned by Salesforce in the original DAHLIA. we're using it as a
-# quick way to get some test listing data while SMC/SJ DAHLIA is in
-# early development.
-sample_listing_file = File.read 'lib/sample_data/sample_listing.json'
-sample_listing_salesforce_fields = JSON.parse(sample_listing_file)
-listing = Listing.new
-listing.add_fields(sample_listing_salesforce_fields, :salesforce)
-Listing.create listing.to_domain.to_hash
+sample_listing_file = File.read 'lib/sample_data/sample_listings.json'
+sample_listings = JSON.parse(sample_listing_file)
+sample_listings.each do |l|
+  Listing.create(l) unless Listing.exists?(listing_id: l['listing_id'])
+end
