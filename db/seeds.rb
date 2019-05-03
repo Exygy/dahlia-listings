656134f510de9e1f76f61f1a7ec9a87d8e484056
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-05-03 09:22:54 -0700

    [#164584674] Remove database setup
    We are consolidating migrations and seed data for the database that is shared between this listing app and the admin app into the admin app only.

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-sample_listing_file = File.read 'lib/sample_data/sample_listings.json'
-sample_listings = JSON.parse(sample_listing_file)
-sample_listings.each do |l|
-  Listing.create(l) unless Listing.exists?(listing_id: l['listing_id'])
-end
