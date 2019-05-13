da3c24087e0c8498b77f3ebc0a5b10c5498a09b5
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-05-13 15:34:11 -0700

    [#165797252] Add setup to only use DB migrations in test
    Because the regional DAHLIA listings site and admin site are sharing a database, we are consolidating migrations into just one app to keep things simple for now. We are running migrations from the admin app, so we will not be keeping migrations in the listings app. For running tests on the listings app, we will generate the migrations from the dahlia_data_models gem for the purposes of creating the test DB, and then delete the migrations when the tests are done.

--- db/migrate/20190513174040_add_unit_related_fields_to_listing.rb
+++
@@ -1,6 +0,0 @@
-class AddUnitRelatedFieldsToListing < ActiveRecord::Migration[5.2]
-  def change
-    add_column :listings, :show_unit_features, :boolean
-    add_column :listings, :unit_amenities, :text
-  end
-end
