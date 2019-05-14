a903328aad7a70d902f273b1d561ae415e1814dd
Catherine Callaghan <catherine.callaghan@exygy.com>
2019-05-14 14:32:38 -0700

    [#165926755] Add more test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,35 @@
 # frozen_string_literal: true
 
-if Group.count.zero?
-  Group.create(
-    name: 'San Mateo County',
-    slug: 'smc',
-    domain: 'herokuapp.com',
-    group_type: :county,
-  )
-  Group.create(
-    name: 'San Jose',
-    slug: 'sj',
-    domain: 'localhost',
-  )
+def parse_from_json_file(filename)
+  json_file_str = File.read "lib/sample_data/#{filename}"
+  JSON.parse(json_file_str)
 end
 
-sample_listing_file = File.read 'lib/sample_data/sample_listings.json'
-sample_listings = JSON.parse(sample_listing_file)
-sample_listings.each do |l|
-  Listing.create(l) unless Listing.exists?(listing_id: l['listing_id'])
+def create_listing_from_file(filename, group)
+  listing_fields = parse_from_json_file(filename)
+  return false if Listing.exists?(listing_id: listing_fields['listing_id'])
+  listing = Listing.create(listing_fields)
+  listing.update_attribute(:group, group)
+end
+
+def create_groups_from_file
+  groups_fields = parse_from_json_file('groups.json')
+  groups_fields.each do |fields|
+    next if Group.exists?(slug: fields['slug'])
+    Group.create(fields)
+  end
+end
+
+# Create the placeholder groups and listings that we're using to represent the
+# real SMC and SJ groups and listings. Once Phase 1 of regional DAHLIA is
+# launched, we will have finalized the actual data for the real SMC and SJ
+# groups and listings and will remove the SMC and SJ placeholder data. We
+# will keep the test data and add more test data.
+create_groups_from_file
+
+%w[test smc sj].each do |slug|
+  create_listing_from_file(
+    "#{slug}_listing.json",
+    Group.find_by(slug: slug),
+  )
 end
--- spec/e2e/features/group_switchable/group_switchable.feature
+++ spec/e2e/features/group_switchable/group_switchable.feature
@@ -3,13 +3,13 @@ Feature: Switching Groups
     I should be able to view the right group site
 
     Scenario: Attempting to view a group site
-      Given I try to view the group homepage
-      Then I should see the correct group site title
+      Given I try to view the SMC group homepage
+      Then I should see the correct SMC group site title
       And I view a different group homepage
       Then I should see the correct second group site title
 
     Scenario: Seeing common content across groups
-      Given I try to view the group homepage
+      Given I try to view the SMC group homepage
       Then I should see a common headline
       And I view a different group homepage
       Then I should see a common headline
