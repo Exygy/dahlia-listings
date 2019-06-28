46fc64ee618e2c11670f74a6fbe8310ca548e47d
Ben Kutler <ben@benkutler.com>
2019-06-28 15:11:12 -0700

    remove extraneous files from previous commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   # config.active_record.migration_error = :page_load
-  config.active_record.migration_error = false
+  config.active_record.migration_error = :page_load
 
   # Suppress output of asset requests
   config.assets.quiet = true
